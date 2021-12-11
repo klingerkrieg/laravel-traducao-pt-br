4b56d5632fb7cec72407a6ebc0922e2c8b923fdf
Alan Klinger <klingerkrieg@gmail.com>
2021-12-10 21:52:15 -0300

    SUAP fail

--- auth.php
+++ auth.php
@@ -2,5 +2,6 @@
 
 return [
     'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+	'suap-failed' => 'A matrícula ou senha do SUAP está incorreta.',
     'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
 ];
